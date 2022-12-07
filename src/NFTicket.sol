3f9729b6102d5ae040e9517a293e493948b97258
Marc Nyiti <nyiti604@gmail.com>
2022-12-07 18:52:46 +0000

    code clean up and bug fixes

--- src/NFTicket.sol
+++ src/NFTicket.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.17;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
-contract TicketNFT is ERC721 {
+contract TicketNFT is ERC721, ReentrancyGuard {
   // Define the contract owner
   address public owner;
 
@@ -22,11 +23,16 @@ contract TicketNFT is ERC721 {
   mapping(uint256 => bool) public redeemed;
 
   // Define ticketId
-  uint256 public ticketId;
+  uint256 public ticketId = 1;
 
   // TODO create events
 
-  constructor(uint256 _ticketPrice, uint256 _totalSupply, string memory _name, string memory _symbol, string memory _uri) public {
+  constructor(
+    uint256 _ticketPrice, 
+    uint256 _totalSupply, 
+    string memory _name, 
+    string memory _symbol, 
+    string memory _uri) public {
     
     owner = msg.sender;
     ticketPrice = _ticketPrice;
@@ -35,28 +41,28 @@ contract TicketNFT is ERC721 {
     symbol = _symbol;
     uri = _uri;
   }
-  
+
   //////PUBLIC FUNCTIONS////// 
 
   // A function for purchasing a ticket
-  function purchaseTicket() public payable {
+  function purchaseTicket() public payable nonReentrant {
     // Checks
     require(msg.value >= ticketPrice, "Insufficient funds");
     require(ticketId <= totalSupply);
 
-    // Create a new NFT token and assign it to the caller
-    ticketId = totalSupply() + 1;
-    _mint(msg.sender, ticketId);
-
     // Add the caller to the array of ticket holders
     ticketHolders[ticketId] = msg.sender;
+
+    // Create a new NFT token and assign it to the caller 
+    _mint(msg.sender, ticketId);
+    ticketId++;
 
     // Transfer the ticket price to the contract owner
     payable(owner).transfer(ticketPrice);
   }
 
   // A function for transferring a ticket
-  function transferTicket(uint256 ticketId, address recipient) public {
+  function transferTicket(uint256 ticketId, address recipient) public nonReentrant {
     // Check if the caller is the current owner of the ticket
     require(_ownerOf(ticketId) == msg.sender, "You are not the current ticket holder");
 
@@ -78,8 +84,9 @@ contract TicketNFT is ERC721 {
 
   // A function for redeeming a ticket
   function redeemTicket(uint256 ticketId) public {
-    // Check if the caller is the current owner of the ticket
+    // Checks
     require(_ownerOf(ticketId) == msg.sender, "You are not the current ticket holder");
+    require(redeemed[ticketId] = false);
 
     // Add to list of redeemed tickets
     redeemed[ticketId] = true;
@@ -91,6 +98,13 @@ contract TicketNFT is ERC721 {
   function setTicketPrice(uint256 _price) public {
     require(msg.sender == owner);
     ticketPrice = _price;
+  }
+
+  // A function to adjust ticket supply
+  function setTicketSupply(uint256 _supply) public {
+    require(msg.sender == owner);
+
+    totalSupply = _supply;
   }
   
 }
